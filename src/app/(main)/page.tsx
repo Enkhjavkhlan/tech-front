98f0b627934f6c3e1dcfbd68052497e9dbbc9f86
javhaa <javhaa.gg@gmail.com>
2025-04-02 04:15:41 +0800

    update

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
   };
   return (
     <div className="flex flex-col flex-1">
+      <div className="bg-primary2 ">qwe qaweq </div>
       <div className="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
         <div>
           <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg">
